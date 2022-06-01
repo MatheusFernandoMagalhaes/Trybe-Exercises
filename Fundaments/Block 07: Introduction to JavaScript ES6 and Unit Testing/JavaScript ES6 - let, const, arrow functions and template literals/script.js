058bc033a4bc1fa47c25481b3491a3d38da64d22
Matheus Fernando <matheusf.magalhaes@hotmail.com>
2022-06-01 15:24:26 -0300

    Realizando exercicios do dia

---
+++ Fundaments/Block 07: Introduction to JavaScript ES6 and Unit Testing/JavaScript ES6 - let, const, arrow functions and template literals/script.js
@@ -0,0 +1,18 @@
+// <!-- 🚀 Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function.
+// Modifique as concatenações para template literals.
+// Copie o código abaixo. -->
+
+function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
---
+++ Fundaments/Block 08: Higher Order Functions of JavaScript ES6/JavaScript ES6 - spread operator, rest parameter, destructuring and more/exercicio1.js
@@ -0,0 +1,17 @@
+// 1 - Dado o código abaixo, complete - o de forma que seja impressa a área dos 3 retângulos.O código deve retornar em sequência 2, 15 e 54.
+// Dica: use spread operator.
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+
+
+console.log(rectangleArea(rectangles));
+// rectangles.forEach((rectangle) => {
+//   rectangleArea() // altere a chamada da funcao rectangleArea
+//   console.log(rectangle[0] * rectangle[1]);
+// });
